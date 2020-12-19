f60738327d466c4aea0ff6d4c8b8d9c62629c44a
mksahota10 <mksahota10@gmail.com>
2020-12-18 18:35:11 -0800

    finished

---
+++ models/terminal.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const Terminal = mongoose.model (
+    "Terminal",
+    new mongoose.Schema({
+        name: String,
+        flights: [],
+        capacity: Number,
+    })
+)
+
+
+module.exports = Terminal
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const dbConfig = require('./config/db.config')
 
 
 const db = require("./models");
+const Flight = require('./models/flight.model');
+const Terminal = require('./models/terminal.model');
 const Airport = db.airport;
 
 // db connection
@@ -26,4 +28,51 @@ const airport = new Airport({
 
 airport.save()
 console.log("Airport saved", airport)
+
+const airport1 = new Airport ({
+  name: "JFK", 
+  country: "USA", 
+  opened: "1990-12-12"
+})
+
+airport1.save()
+console.log("Airport saved", airport1)
+
 // Lets Make and Save our first airport
+//create into server 
+//saving documents into other documents 
+
+      const flight1 = new Flight({
+        from: "CDG France", 
+        to: "'JFK New York, USA", 
+        airline: 'American Airlines'
+      
+      })
+      flight1.save()
+      console.log("Flight saved", flight1)
+      
+      const flight2 = new Flight({
+        from: "Heathrow UK", 
+        to: "JFK New-York, USA", 
+        airline: "British Airlines"
+      })
+      flight2.save()
+      console.log("Flight saved", flight2)
+
+//
+      // Lets Make and Save our first airport
+
+      //new terimanil, pass all the variables in the model 
+
+      const terminal1 = new Terminal ({
+        name:"JFK",
+        capacity: "234324"
+      })
+      //pushing flights 1 and 2 into flights 
+      terminal1.flights.push(flight1);
+      terminal1.flights.push(flight2);
+      terminal1.save()
+      console.log("terminal saved", terminal1)
+
+
+
